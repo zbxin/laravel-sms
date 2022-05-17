2384074bf7630364a0a26d08efdb3e8f6ef8c193
zbxin <94103363+zbxin@users.noreply.github.com>
2022-05-17 10:00:43 +0800

    Update ZhMobileValidator.php
    修改手机号码验证正则

--- src/Validators/ZhMobileValidator.php
+++ src/Validators/ZhMobileValidator.php
@@ -8,6 +8,6 @@ class ZhMobileValidator extends Validator
 {
     public function validator($attribute, $value, $parameters, $validator)
     {
-        return preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[5678]|18\d|16\d|19\d)\d{8}|170[059]\d{7})$/', $value);
+        return preg_match('/^(\+?0?86\-?)?((13\d|14[57]|15[^4,\D]|17[35678]|18\d|16\d|19\d)\d{8}|170[059]\d{7})$/', $value);
     }
 }
